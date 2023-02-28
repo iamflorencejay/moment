ce7fe598a8a6b075e859df9d8a3f5c49e6e9b87e
Rave | 図書館猫 <raphael.wickihalder@odysee.com>
2023-02-28 19:48:40 +0100

    Fix locale path

--- min/moment-with-locales.js
+++ min/moment-with-locales.js
@@ -2093,7 +2093,7 @@
             try {
                 oldLocale = globalLocale._abbr;
                 aliasedRequire = require;
-                aliasedRequire('./locale/' + name);
+                aliasedRequire('../locale/' + name);
                 getSetGlobalLocale(oldLocale);
             } catch (e) {
                 // mark as not found to avoid repeating expensive file require call causing high CPU
